237d2403843c3843bf45929f43c64e28bfa22d22
Hewad Noori <hewadnoori16@gmail.com>
2022-06-10 15:15:23 -0400

    first custom component!

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById('root'))
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById('root'));
+ReactDOM.render(
+    <ul>
+        <li>first</li>
+        <li>second</li>
+    </ul>, document.getElementById('root'));
+//first custom component in react!
+function MainContent() {
+    return (
+        <div>
+            <p>first component</p>
+            <h3>this is my first component.</h3>
+            <p>components can only have one parent elements but unlimited child elements so just wrap everything in a div!</p>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <MainContent />
+
+    </div>,
+    document.querySelector('#root')
+)
