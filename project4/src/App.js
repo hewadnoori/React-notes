8b47b2ffde7813726f1a0474a6f70389b58f8ab7
Hewad Noori <hewadnoori16@gmail.com>
2022-07-11 16:04:49 -0400

    added more to first prop

--- project4/src/App.js
+++ project4/src/App.js
@@ -5,8 +5,22 @@ import Joke from './Joke';
 export default function App() {
     return (
         <div>
-            <Joke setup="I got my daughter a fridge for her birthday" punchline="I can't wait to see her face light up when she opens it" />
-            <Joke setup="How did the hacker escape the police?" punchline="He just ransomeware!" />
+            <Joke
+                setup="I got my daughter a fridge for her birthday"
+                punchline="I can't wait to see her face light up when she opens it"
+                isPun={true}
+                upvotes={10}
+                downvotes={2}
+                comments={[{ author: '', body: "", titles: "" }, { img: "", date: "" }]}
+            />
+            <Joke
+                setup="How did the hacker escape the police?"
+                punchline="He just ransomeware!"
+                isPun={true}
+                upvotes={10}
+                downvotes={2}
+                comments={[{ author: '', body: "", titles: "" }, { img: "", date: "" }]}
+            />
         </div>
     )
 }
