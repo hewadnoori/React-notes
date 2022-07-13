63a697ee4fda99ec1257de505c1e8c2ef37c2402
Hewad Noori <hewadnoori16@gmail.com>
2022-07-13 08:38:12 -0400

    notes

--- react-folder/src/index.js
+++ react-folder/src/index.js
@@ -10,3 +10,23 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const nums = [1, 2, 3, 4, 5];
+const squared = nums.map(function (item) {
+  return item * item;
+})
+console.log(squared);
+
+const names = ["alice", "bob", "charlie", "danielle"]
+const uppercaseNames = names.map((item) => {
+  var char = item[0].toUpperCase() + item.slice(1);
+  return char;
+
+})
+console.log(uppercaseNames);
+
+const pokemon = ['Bulbasaur', 'Charmander', 'Squirtle']
+const newPokemon = pokemon.map((mon) => {
+  return `<p>${mon}<p>`
+})
+console.log(newPokemon);
